4de6d6afee7b15bdbf8e27f8fa6ae92310dccd15
kxhit <357787252@qq.com>
2024-06-12 15:42:41 +0100

    gpu to numpy

--- app.py
+++ app.py
@@ -382,20 +382,9 @@ def get_reconstructed_scene(filelist, schedule, niter, min_conf_thr,
     if mode == GlobalAlignerMode.PointCloudOptimizer:
         loss = scene.compute_global_alignment(init='mst', niter=niter, schedule=schedule, lr=lr)
 
-    # outfile = get_3D_model_from_scene(outdir, silent, scene, min_conf_thr, as_pointcloud, mask_sky,
-    #                                   clean_depth, transparent_cams, cam_size, same_focals=same_focals)
-
-    # also return rgb, depth and confidence imgs
-    # depth is normalized with the max value for all images
-    # we apply the jet colormap on the confidence maps
-    rgbimg = scene.imgs
-    # depths = to_numpy(scene.get_depthmaps())
-    # confs = to_numpy([c for c in scene.im_conf])
-    # cmap = pl.get_cmap('jet')
-    # depths_max = max([d.max() for d in depths])
-    # depths = [d / depths_max for d in depths]
-    # confs_max = max([d.max() for d in confs])
-    # confs = [cmap(d / confs_max) for d in confs]
+    # for eschernet
+    cams2world = to_numpy(scene.get_im_poses().cpu())
+    rgbimg = to_numpy(scene.imgs)
 
     imgs = []
     rgbaimg = []
@@ -412,12 +401,6 @@ def get_reconstructed_scene(filelist, schedule, niter, min_conf_thr,
             rgbaimg.append(np.array(imgs_rgba[i]))
 
     rgbaimg = np.array(rgbaimg)
-
-    # for eschernet
-    # get optimized values from scene
-    rgbimg = to_numpy(scene.imgs)
-    # focals = to_numpy(scene.get_focals().cpu())
-    cams2world = to_numpy(scene.get_im_poses().cpu())
 
     # 3D pointcloud from depthmap, poses and intrinsics
     pts3d = to_numpy(scene.get_pts3d())
